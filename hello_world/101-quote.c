6408015e73b2ef82dc43369c23bc91eb9fa113e9
Tito <xtremegt@hotmail.com>
2023-10-13 09:43:01 -0400

    quote with write test

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,6 +6,6 @@ include<stdio.h>
  */
 int main(void)
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n);
+	fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n);
 	return (1);
 }
