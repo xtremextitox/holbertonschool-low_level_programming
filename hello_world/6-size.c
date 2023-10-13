12ec5c5a68ec1a6e642e25561675afb30854f62e
Tito <xtremegt@hotmail.com>
2023-10-12 20:01:33 -0400

    size of different types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,12 +3,12 @@
  * main - Printing the size and compiled to run
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 printf("Size of char: %zu bytes\n", sizeof(char));
 printf("Size of int: %zu bytes\n", sizeof(int));
 printf("Size of long int: %zu bytes\n", sizeof(long int));
 printf("Size of long long int: %zu bytes\n", sizeof(long long int));
 printf("Size of float %zu bytes\n", sizeof(float));
-return 0;
+return (0);
 }
