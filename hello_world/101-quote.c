cbde469a3890fa6332df758aa4eabc1c7952c788
Tito <xtremegt@hotmail.com>
2023-10-13 09:46:58 -0400

    quote with write test

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
- * main - Prints exactly a line followed by a line and 
- * standard error
+ * main - Prints exactly a line followed by a line and standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fwrite(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19"\n);
+	fwrite(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
