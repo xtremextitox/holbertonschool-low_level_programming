2912bb4d5c1fea3a515b756bd1ebe317c67f1e4f
Tito <xtremegt@hotmail.com>
2023-10-16 19:43:35 -0400

    printing 1 to 0 with putchar, i need to review it

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,14 +6,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-
-	int c;
-
-	for (c = '0'; c <= '9'; c++)
-		putchar(c);
-		putchar('\n');
-
-	return (0);
+int c;
+for (c = '0'; c <= '9'; c++)
+putchar(c);
+putchar('\n');
+return (0);
 }
