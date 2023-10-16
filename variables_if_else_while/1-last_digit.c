439cd2d359befd351be7e7738995010904787fe7
Tito <xtremegt@hotmail.com>
2023-10-16 12:44:44 -0400

    learning how to print the last digit of the number stored

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,15 +14,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int lastDigit(int n)
-
-	if (lastDigit > 5)
+	if ((n % 10) > 5)
 		printf("Last digit of %d and is greater than 5\n", n);
 
-	else if (lastDigit == 0)
+	else if ((n % 10)  == 0)
 		printf("Last digit of %d and is 0\n", n);
 
-	else if (lastDigit < 6)
+	else if ((n % 10) < 6)
 		printf("Last digit of %d and is less than 6 and not 0\n", n);
 	return (0);
 }
