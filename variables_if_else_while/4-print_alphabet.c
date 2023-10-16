a8c8279143695dd6becf720aa3b507a17eddf9bc
Tito <xtremegt@hotmail.com>
2023-10-16 14:20:30 -0400

    all letters printed except 2

---
+++ variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Printing the alphabet in lower case while not printing q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		if (c = 'e' && c != 'q')
+		putchar(c);
+		putchar('\n');
+		return (0);
+}
