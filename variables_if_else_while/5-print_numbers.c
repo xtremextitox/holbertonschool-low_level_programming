b17e047331e74dd6684b0e0bf28a33bae99066a7
Tito <xtremegt@hotmail.com>
2023-10-16 14:40:18 -0400

    printing numbers 1 to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Printing numbers from 0 to 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '1'; c <= '10'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
