c0a846e9709629c7a022def5e4c3a76d751633f5
Tito <xtremegt@hotmail.com>
2023-10-16 13:28:06 -0400

    learning how to do alphabet print

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Printing the alphabet, coming back to change it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++) {
+}
+putchar('\n');
+return (0);
+}
