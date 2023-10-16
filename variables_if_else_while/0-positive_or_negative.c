5aa93f98e950dc3173a3fca8e9e09deebb8d9e74
Tito <xtremegt@hotmail.com>
2023-10-16 10:52:18 -0400

    learning if with positive and negative task

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -6,10 +6,11 @@
  * main - Positive or negative task to learn how to use if and else
  * Return: Always 0 (Success)
  */
-  int main(void)
+int main(void)
 {
 	int n;
 	srand(time(0));
+	
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	printf("%d is positive\n", n);
