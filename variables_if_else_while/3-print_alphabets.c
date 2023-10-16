36c5d62ac520ba04e27282c516c628d29865bfa7
Tito <xtremegt@hotmail.com>
2023-10-16 13:55:45 -0400

    Lower case and Upper case alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Printing the alphabet in lower case then upper case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char C;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (C = 'A'; C <= 'Z'; C++)
+		putchar(C);
+	return (0);
+}
