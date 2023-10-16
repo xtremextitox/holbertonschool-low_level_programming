cf145a7be9c9c919dff13918c7a5be06df0f7fce
Tito <xtremegt@hotmail.com>
2023-10-16 18:48:41 -0400

    printing numbers 1 to 10

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -6,12 +6,11 @@
  */
 int main(void)
 {
-	char c;
-	for (c = 1; c <= 9; c++)
-	{
-		putchar(c);
-	}
-		putchar('\n');
-	
-	return (0);
-	}
+char c;
+for (c = 0; c <= 9; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
