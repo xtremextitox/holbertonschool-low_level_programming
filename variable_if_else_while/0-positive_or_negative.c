c1969f454e8dbfab86196c50931d0c1b6de97395
Tito <xtremegt@hotmail.com>
2023-10-16 09:59:29 -0400

    learning if with positive and negative task

---
+++ variable_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Positive or negative task to learn how to use if and else
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+if (n > 0)
+{
+	printf(is positive);
+}
+if (n = 0)
+{
+	printf(is zero);
+}
+if (n < 0)
+{
+	printf(is negative)
+}
+return (0);
+}
---
+++ variable_if_else_while/README.md
@@ -0,0 +1,1 @@
+Learning about For, While and other codes
