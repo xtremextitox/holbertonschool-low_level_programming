5860e1b166f64422a46ac114bb5d65945417b92c
Tito <xtremegt@hotmail.com>
2023-10-16 20:03:52 -0400

    Printing reverse alph

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
