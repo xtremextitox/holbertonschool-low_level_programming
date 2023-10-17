79e0b7643d29cd282826a49a8351ebd3def09c94
Tito <xtremegt@hotmail.com>
2023-10-17 12:35:27 -0400

    main.h alphabet testing

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - printing the alphabet in lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	_putchar(c = 'a'; c <= 'z'; c++)
+		_putchar(c'\n');
+	return (0);
+}
