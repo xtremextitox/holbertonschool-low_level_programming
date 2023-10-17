6cdb7c8a11f15c5cf4528eeededecb7a09b50ec1
Tito <xtremegt@hotmail.com>
2023-10-17 11:22:14 -0400

    main.h learning

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
+#include <main.h>
 /**
- * main - Printing _putchar
+ * main - Learning to putchar with main.h
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("_putchar,\n");
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
 }
