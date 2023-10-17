c4efc02dd3133ea93953c43fd85718c2f07ddee5
Tito <xtremegt@hotmail.com>
2023-10-16 20:06:34 -0400

    Printing reverse alph

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	char c;
-	for (c = 'z'; c >= 'a'; c--)
-	putchar(c);
-	putchar('\n');
-	return (0);
+char c;
+for (c = 'z'; c >= 'a'; c--)
+putchar(c);
+putchar('\n');
+return (0);
 }
