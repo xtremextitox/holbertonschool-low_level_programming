0aeed27f7126bd3544c0564bef727e6e79d0300d
Tito <xtremegt@hotmail.com>
2023-10-17 08:59:52 -0400

    print combination

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Printing all possible combinations of a single digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char d;
+int c;
+for (c = '0'; c <= '9'; c++)
+putchar(c);
+putchar(,);
+putchar('/n');
+return (0);
+}
+
