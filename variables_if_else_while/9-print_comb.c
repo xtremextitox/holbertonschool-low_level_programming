97b7b786f34994aa7c2615115db93e54c83c70ec
Tito <xtremegt@hotmail.com>
2023-10-17 09:08:13 -0400

    print combination

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,14 +5,18 @@
  * main - Printing all possible combinations of a single digit numbers
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 int c;
 for (c = '0'; c <= '9'; c++)
-putchar(c);
 {
-putchar(,);
-putchar( );
+putchar(c);
+if (c != '9')
+{
+putchar(' ');
+putchar('.' );
+}
 }
 putchar('\n');
 return (0);
