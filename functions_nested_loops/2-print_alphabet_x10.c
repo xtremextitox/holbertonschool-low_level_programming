d38ac108e37627f393fd3de7a7733fb769829a83
Tito <xtremegt@hotmail.com>
2023-10-17 14:44:21 -0400

    10x alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,17 @@
  */
 void print_alphabet_x10(void)
 {
-	char c;
-	int i;
-	i = 0;
+char c;
+int i;
+i = 0;
 
-	while (i < 10)
-	{
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
-	c++;
+while (i <= 10)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+c++;
 }
 }
