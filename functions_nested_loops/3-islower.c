2a81606f7d68b258b861f0f1f9f6512f077e6961
Tito <xtremegt@hotmail.com>
2023-10-18 12:11:38 -0400

    lowercase count

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - Function that checks for lowercase character
- * c: for check character purposes
+ * @c: for check character purposes
  * Return: 1 for lowercase character or 0 for everything
  */
 int _islower(int c)
