024b56b1ce04cb948985afdc3e09d273a342057f
Tito <xtremegt@hotmail.com>
2023-10-19 09:57:13 -0400

    adding two integers and return the result

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,6 +3,7 @@
  * add - Function that adds two integers and return the result.
  * @n1 - First number to be compute
  * @n2 - Second number to be compute
+ * Return: Integer value
  */
 int add(int n1, int n2)
 {
